3e9ef3470c941bf7a07ad86b4f03cb9d6fa1e391
Andrew Shaffer <andrew@weo.io>
2015-06-23 12:07:20 -0700

    swap index/server js files and remove procfile

--- server.js
+++ server.js
@@ -1,23 +1,2 @@
-/**
- * Modules
- */
-import {port} from './lib/config'
-import koa from 'koa'
-import mount from 'koa-mount'
-import main from './lib/main'
-
-/**
- * Vars
- */
-const app = koa()
-
-/**
- * Main
- */
-app.use(mount(main))
-
-/**
- * Listen
- */
-app.listen(port)
-console.log('listening on port', port)
+require('babel/register')
+require('./index')
